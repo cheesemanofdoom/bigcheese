5c5b16e4f97936c2f0240f66db70ec44acb59c08
cheesemanofdoom <110896512+cheesemanofdoom@users.noreply.github.com>
2022-08-09 15:07:32 +0000

    Update script.js

--- script.js
+++ script.js
@@ -2,39 +2,35 @@ var clear1st = document.querySelector(".js-name-form-submit")
 
 var container = document.querySelector(".js-container")
 
-clear1st.addEventListener("click", function (event) {
-  event.preventDefault()
-  localStorage.setItem("1st", " ")
-})
-
 var cheeselist =
   "cheddar, parmigiano reggiano, parmesan, roquefort, brie, gruyere, feta, mozzarella, manchego, gorgonzola, epoisses, swiss cheese, gouda cheese, monterey jack, muenster cheese, provolone, blue cheese, camembert, irish cheddar, havarti, buffalo mozzarella, colby-jack, monterey jack, edam, ricotta, manchego, pepper jack cheese, roquefort, goat cheese, colby cheese, stilton cheese, emmentaler, cream cheese, comté cheese, american farmhouse cheddar, cottage cheese, grana padano, halloumi, rembrandt gouda, sharp provolone, danish blue, saint albray, appenzeller cheese, mascarpone, tomme de savoie, string cheese, saint-marcellin, american cheese, pont-l'évêque cheese, mahón cheese, cantal cheese, emmental français, selles-sur-cher cheese, neufchâtel cheese, morbier cheese, fourme de montbrison, cheese curdcaprice des dieux, reblochon, coulommiers cheese, taleggio cheese, vacherin, beaufort cheese, beaufort alpage, abondance cheese, saint-paulin cheese, chaource cheese, livarot cheese, rocamadour cheese, cambozola, oregon blue vein, leonora goat cheese, pimento cheese, banon cheese, cabrales cheese, tête de moine, boerenkaas, emmental de savoie, brocciu, laguiole cheese, limburger cheese, queso de bola, maroilles cheese, stichelton, ossau-lraty cheese, saint-nectaire, pont l'evêque, crottin de chavignol, mimolette, pélardon, fourme d'ambert, fromage frais, bleu du vercors-sassenage, cabecou, saint-félicien cheese, bleu d'auvergne, bleu de bresse, farmer cheese, hooligan, bleu des causses"
 
-localStorage.setItem("2nd", "")
+const cheeseadded = []
 
-localStorage.setItem("3rd", "")
-
-localStorage.setItem("1st", "")
 var nameoffield = document.querySelector(".js-name-form-field")
+
 var btnSubmit = document.querySelector(".js-name-form-submit")
 
 function cheeseadd() {
   nameoffield = document.querySelector(".js-name-form-field")
+  var lowered = nameoffield.value.toLowerCase()
   //if cheese in cheeselist
-  let result = cheeselist.includes(nameoffield.value.toLowerCase())
+  let result = cheeselist.includes(lowered)
   if (result == true) {
-    container.innerHTML +=
-      `
-  <div>
-    <h2>` +
-      nameoffield.value +
-      `</h2>
-  </div>
-  `
+    if (cheeseadded.includes(lowered) == false) {
+      container.innerHTML +=
+        `
+          <div>
+            <h2>` +
+        nameoffield.value +
+        `</h2>
+          </div>
+          `
+      cheeseadded.push(lowered)
+    }
   } else {
     window.location = "https://www.youtube.com/watch?v=xvFZjo5PgG0"
   }
-  //else: rickroll
 }
 
 btnSubmit.addEventListener("click", function (event) {
@@ -48,11 +44,6 @@ cheeseblog.addEventListener("click", function (event) {
   event.preventDefault()
   window.location = "https://www.cheese.com/"
 })
-
-function sayHello(name) {
-  console.log("Hello," + name)
-}
-for (i = 0; i < 2; i++) sayHello("")
 
 var cheesevideo = document.querySelector(".js-name-form-cheesevideo")
 
